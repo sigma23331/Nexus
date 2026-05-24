297a5bbe2d71401da11fc27ac557b4a565a7a794
liu458507 <liu458507@gmail.com>
2026-05-24 20:07:51 +0800

    feat: 设计了默认头像的svg与png透明底图像

---
+++ frontend/src/components/icons/IconDefaultAvatar.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg class="h-5 w-5" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="50" cy="50" r="40" fill="#F0F4FF" />
+    <circle cx="50" cy="50" r="40" stroke="#000000" stroke-width="2" />
+    <circle cx="50" cy="40" r="14" fill="#000000" />
+    <path
+      d="M26 71C26 62.1634 33.1634 55 42 55H58C66.8366 55 74 62.1634 74 71V73H26V71Z"
+      fill="#000000"
+    />
+  </svg>
+</template>
